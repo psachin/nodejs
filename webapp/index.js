0df380c646e6ed7a0c3376bd73cae61c8debfea7
psachin <iclcoolster@gmail.com>
2013-08-10 11:58:42 +0530

    Subject: module implementation
    Description:
    
    - index.js for bootstrapping and starting server

---
+++ webapp/index.js
@@ -0,0 +1,4 @@
+// index.js
+// for bootstrapping and starting a server
+var server = require("./server");
+server.start();
--- webapp/server.js
+++ webapp/server.js
@@ -1,15 +1,22 @@
 // the simplest http server listening at PORT 8888
-
 var http = require("http");
 
 // custom onRequest function to take request and respond with
 // "hello world"
-function onRequest(request,response){
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write("Hello world");
-    response.end();
+// implementing server as Node.js module
+function start() {
+    function onRequest(request,response){
+	console.log("Request received");
+	response.writeHead(200, {"Content-Type":"text/plain"});
+	response.write("Hello world");
+	response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log("server listening at http://localhost:8888/");
 }
+// export function start() as start
+exports.start = start;
 
-http.createServer(onRequest).listen(8888);
 
-console.log("server listening at http://localhost:8888/");
+
+
