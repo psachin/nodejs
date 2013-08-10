250cfb71d11ca40a1a34e3d907eae4d1d4fb3a33
psachin <iclcoolster@gmail.com>
2013-08-10 11:41:23 +0530

    Subject: anonymous function to createServer()
    Description:

--- webapp/server.js
+++ webapp/server.js
@@ -1,7 +1,13 @@
 // the simplest http server listening at PORT 8888
 
 var http = require("http");
-var server = http.createServer();
-server.listen(8888);
+
+// we pass createServer an anonymous function to take request and respond with
+// "hello world"
+http.createServer(function(request,response){
+    response.writeHead(200, {"Content-Type":"text/plain"});
+    response.write("Hello world");
+    response.end();
+}).listen(8888);
 
 console.log("server listening at http://localhost:8888/");
